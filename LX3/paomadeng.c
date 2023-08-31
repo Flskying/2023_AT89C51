924ec94080e648c73a37dd1d0d7e778e2eedd8d1
XingYunCao <75520122+Flskying@users.noreply.github.com>
2023-08-31 18:26:44 +0800

    Add files via upload
    第一周的单片机作业
    LX1：亮灯
    LX2：闪烁
    LX3：跑马灯
    LX4：闪停
    LX5：加按钮
    LX6：加开关

---
+++ LX3/paomadeng.c
@@ -0,0 +1,22 @@
+#include <reg51.h>
+#define uchar unsigned char
+void delay()
+{
+uchar i,j;
+for(i=0;i<255;i++)
+for(j=0;j<255;j++);
+}
+
+void main()
+{
+int m;
+P1 = 1;
+while(1) {
+ for(m = 0; m < 8; m++) 
+ {
+    P1 = ~(1 << m); 
+    delay();
+ }
+		}			    
+}
+ 
---
+++ LX6/LX1.c
@@ -0,0 +1,5 @@
+ #include <reg51.h>
+ void main()
+ {
+ 	P1=0;
+ }
